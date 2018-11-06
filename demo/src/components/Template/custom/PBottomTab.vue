9a5001c9e45456ec46e1fd593226a7ef48cf5481
Jiang Feng MAO <maojiangfeng00@outlook.com>
2018-11-06 17:25:02 +0800

    add PBottomTab module for page bottom

---
+++ demo/src/components/Template/custom/PBottomTab.vue
@@ -0,0 +1,28 @@
+<template>
+    <tabbar>
+        <tabbar-item :selected="index == 0" v-for="(item, index) in list" :key="index" :link='item.link'>
+            <img slot="icon" :src="item.img">
+            <span slot="label">{{item.text}}</span>
+        </tabbar-item>
+    </tabbar>
+</template>
+
+<script>
+import { Tabbar, TabbarItem } from 'vux'
+export default {
+    components:{
+        Tabbar, 
+        TabbarItem
+    },
+    props:{
+        list:{
+            type:Array,
+            default:()=>[]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
